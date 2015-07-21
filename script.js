019d22cd385bb0aa32f4aed8e34d8fed8ce68da3
Corné Driesprong <cdriesprong@gmail.com>
2015-07-21 14:08:50 +0200

    colors

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
     $content = $header.next();
     $content.slideToggle(300, function () {
         $header.html(function () {
-            return $content.is(":visible") ? "<h3 class='skills_h3'><span>&nbsp; skills <i class='fa fa-angle-up'></i> &nbsp;</span></h3>" : "<h3 class='skills_h3'><span>&nbsp; skills <i class='fa fa-angle-down'></i> &nbsp;</span></h3>";
+            return $content.is(":visible") ? "<h3 class='skills_h3'><span>skills <i class='fa fa-angle-up'></i></span></h3>" : "<h3 class='skills_h3'><span>skills <i class='fa fa-angle-down'></i></span></h3>";
         });
     });
   });
@@ -15,7 +15,7 @@ $(document).ready(function(){
     $content = $header.next();
     $content.slideToggle(300, function () {
         $header.html(function () {
-            return $content.is(":visible") ? "<h3 class='experience_h3'><span>&nbsp; experience <i class='fa fa-angle-up'></i> &nbsp;</span></h3>" : "<h3 class='experience_h3'><span>&nbsp; experience <i class='fa fa-angle-down'></i> &nbsp;</span></h3>";
+            return $content.is(":visible") ? "<h3 class='experience_h3'><span>experience <i class='fa fa-angle-up'></i></span></h3>" : "<h3 class='experience_h3'><span>experience <i class='fa fa-angle-down'></i></span></h3>";
         });
     });
   });
@@ -25,7 +25,7 @@ $(document).ready(function(){
     $content = $header.next();
     $content.slideToggle(300, function () {
         $header.html(function () {
-            return $content.is(":visible") ? "<h3 class='education_h3'><span>&nbsp; education <i class='fa fa-angle-up'></i> &nbsp;</span></h3>" : "<h3 class='education_h3'><span>&nbsp; education <i class='fa fa-angle-down'></i> &nbsp;</span></h3>";
+            return $content.is(":visible") ? "<h3 class='education_h3'><span>education <i class='fa fa-angle-up'></i></span></h3>" : "<h3 class='education_h3'><span>education <i class='fa fa-angle-down'></i></span></h3>";
         });
     });
   });
@@ -35,7 +35,7 @@ $(document).ready(function(){
     $content = $header.next();
     $content.slideToggle(300, function () {
         $header.html(function () {
-            return $content.is(":visible") ? "<h3 class='portfolio_h3'><span>&nbsp; work <i class='fa fa-angle-up'></i> &nbsp;</span></h3>" : "<h3 class='portfolio_h3'><span>&nbsp; work <i class='fa fa-angle-down'></i> &nbsp;</span></h3>";
+            return $content.is(":visible") ? "<h3 class='portfolio_h3'><span>work <i class='fa fa-angle-up'></i></span></h3>" : "<h3 class='portfolio_h3'><span>work <i class='fa fa-angle-down'></i> &nbsp;</span></h3>";
         });
     });
   });
